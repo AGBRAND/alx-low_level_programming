6f126a3dcb20f5ffd835c6a0c2002732dd9dd638
AGBOR BRANDON <agborbrand@gmail.com>
2022-09-23 09:18:10 -0700

    complete

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - displays text
+ *
+ * Return: Always 0 (validated)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
