fc629f93e20767bfd67a27bf609c038a2e4f313f
AGBRAND <agborbrand@gmail.com>
2023-03-31 13:12:26 -0700

    list

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(Atortoise,havingprettygood sense ofahare's nature, challenges one to a race.)\n");
+	return (0);
+}
